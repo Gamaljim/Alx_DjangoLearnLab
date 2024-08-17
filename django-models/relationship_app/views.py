0c48151a3e4e5b65d7a26ea95a1e82ddf597a06e
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-17 19:02:11 +0400

    task 3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -38,8 +38,7 @@ def is_admin(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admins.html')
-
+    return render(request, 'relationship_app/admin_view.html')
 
 
 def is_member(user):
@@ -48,8 +47,7 @@ def is_member(user):
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request , 'relationship_app/member_view.html')
-
+    return render(request, 'relationship_app/member_view.html')
 
 
 def is_librarian(user):

858c5f81269caca9ab9a827240481a03c10a7971
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-17 19:19:30 +0400

    task 4

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -63,3 +63,13 @@ def librarian_view(request):
 @permission_required('relationship_app.can_add_book')
 def can_add_book(request):
     return render(request, 'relationship_app/add_book.html')
+
+
+@permission_required('relationship_app.can_change_book')
+def can_change_book(request):
+    return render(request, 'relationship_app/change_book.html')
+
+
+@permission_required('relationship_app.can_delete_book')
+def can_delete_book(request):
+    return render(request, 'relationship_app/delete_book.html')

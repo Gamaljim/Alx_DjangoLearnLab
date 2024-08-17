d7ac5c782920faf7373d3e7b2d112a3c55298f03
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-17 19:16:14 +0400

    task 4

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -16,6 +16,13 @@ class Book(models.Model):
     title = models.CharField(max_length=180)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = (
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book')
+        )
+
 
 class Library(models.Model):
     name = models.CharField(max_length=180)
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import UserProfile
 from django.contrib.auth import login, logout
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 
@@ -57,3 +57,4 @@ def is_librarian(user):
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
+

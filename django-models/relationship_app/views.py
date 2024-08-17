f5461c4d94e7b786bf6cbd8690c4de15116cc0ec
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-17 15:41:25 +0400

    renaming project

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
+
 
 # Create your views here.
+
+
+def books(request):
+    all_books = Book.objects.all()
+    return render(request, 'books.html', {'all_books': all_books})

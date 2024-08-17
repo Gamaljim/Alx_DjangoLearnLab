96ba0b1f037c03bafbb5dcebcdd16f9a0767281b
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-17 18:26:14 +0400

    task 3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from .models import Book
 from .models import Library
+from .models import UserProfile
 from django.contrib.auth import login,logout
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
@@ -26,3 +28,11 @@ def register(request):
         form = UserCreationForm()
 
     return render(request , 'relationship_app/register.html', {'form':form})
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admins.html')

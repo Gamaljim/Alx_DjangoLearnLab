19ac38173cd2c186cccf316c8f19c19980ca92e8
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-17 16:32:00 +0400

    task 2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView , signup_user
 
 urlpatterns = [
     path('books/', list_books, name='all_books'),
-    path('library/<int:pk>', LibraryDetailView.as_view(), name='library')
+    path('library/<int:pk>', LibraryDetailView.as_view(), name='library'),
+    path('register', signup_user, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
@@ -14,3 +15,14 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+
+def signup_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
+    return render(request , 'relationship_app/register.html', {'form':form})

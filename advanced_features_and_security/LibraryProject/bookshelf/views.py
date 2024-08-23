b7404f5440f2daddd236cabb2d4f95da53762000
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-23 18:27:38 +0400

    task 0

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from .models import Book
+from .models import CustomUser
+
 
 # Create your views here.
+
+@permission_required("bookshelf.can_edit")
+def book_list(request):
+    return render(request, 'book_list.html')

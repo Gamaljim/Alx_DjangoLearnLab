c2a5e9c16ebb4f6e60d807290adbf04f02228a5d
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-08-24 01:31:36 +0400

    task2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
 from .models import CustomUser
+from .forms import BookForm
 
 
 # Create your views here.
@@ -10,13 +11,23 @@ from .models import CustomUser
 def book_list(request):
     return render(request, 'book_list.html')
 
+
 @permission_required("bookshelf.can_create", raise_exception=True)
 def add_book(request):
-    return render(request, 'add_book.html')
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BookForm()
+
+    return render(request, 'add_book.html', {'form': form})
+
 
 @permission_required("bookshelf.can_view", raise_exception=True)
 def view_books(request):
     return render(request, 'all_books.html')
+
 
 @permission_required("bookshelf.can_delete", raise_exception=True)
 def delete_books(request):

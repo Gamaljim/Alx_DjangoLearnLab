ca0ef19b281d8e39a078ec77954985b01fb64a01
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-01 03:40:45 +0400

    task 3 week12

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 #from .views import BookList
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
@@ -10,4 +12,5 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     #path('api/books/', BookList.as_view(), name='books'),
+    path('api-token-auth', obtain_auth_token, name='api-token-auth')
 ]

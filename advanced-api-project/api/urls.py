e759466a5d75fa48211281188f027070e718f69e
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-06 19:02:40 +0400

    task 1 week 13

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name="books-list"),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name="books-create"),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name="books-update"),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name="books-delete"),
+    path('books/update/', BookUpdateView.as_view(), name="books-update"),
+    path('books/delete/', BookDeleteView.as_view(), name="books-delete"),
 ]

444bcf1eb0e8ed2e95af2898a614f629ba06a3d3
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-06 19:02:12 +0400

    task 1 week 13

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,6 +8,7 @@ from .views import BookDeleteView
 urlpatterns = [
     path('books/', BookListView.as_view(), name="books-list"),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name="books-create"),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name="books-update"),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name="books-delete"),
 ]

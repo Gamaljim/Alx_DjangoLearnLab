e62e945a90683ba609772c323e50c31b2fd371c8
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-07 03:21:47 +0400

    task 3

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
-
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Book, Author
 # Create your tests here.
+
+class UnitListTestCase(APITestCase):
+    def setUp(self):

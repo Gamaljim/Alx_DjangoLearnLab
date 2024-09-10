df331e596e05137b07a9e4348731478590acd90a
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-10 16:47:41 +0400

    task1 intital commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', ),
+    path('profile/'),
 ]
 

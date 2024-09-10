bfbdc4a17622474bc4abf696f8ccebae1214f834
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-11 00:24:05 +0400

    task1 intital commit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -22,12 +23,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
-@receiver(post_save,sender=User)
-def create_profile(sender,instance,created, **kwargs):
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
- @receiver(post_save, sender=User)
-def save_profile(sender,instance,**kwargs):
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
     instance.profile.save()

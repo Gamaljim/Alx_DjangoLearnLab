2dd066ef06f9e26b2146e1fb127e71e8d663ccf6
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-10 17:02:01 +0400

    task1 intital commit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,3 +4,15 @@ from django import forms
 
 
 class CustomerUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.emaiol = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import CustomerUserCreationForm
+from .models import Post
+from django.views.generic import CreateView
+
 
 # Create your views here.
+class RegisterView(CreateView):
+    form_class = CustomerUserCreationForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
+

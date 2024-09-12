2e8d9d2c29949620f692fe5554de1b79bb3b38d1
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-12 18:05:57 +0400

    task 2 full CRUD and documentation

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Profile, Post
+from .models import Profile, Post, Comment
 
 
 class CustomerUserCreationForm(UserCreationForm):
@@ -64,3 +64,9 @@ class PostCreateEditForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -48,3 +48,14 @@ def save_profile(sender, instance, **kwargs):
         Signal to save the Profile instance whenever a User instance is saved.
     """
     instance.profile.save()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'

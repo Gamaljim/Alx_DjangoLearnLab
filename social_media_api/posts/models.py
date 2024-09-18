ae9963d00a540cdade2c9cdab7aafb7e70be9b6c
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-18 20:07:45 +0400

    task 1 first commit

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from accounts.models import CustomUser
+
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=180)
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')

a1d5c25b5ead5e24ab0829e81ec635b776ecf1fe
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-18 21:07:17 +0400

    task 1 first commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
 # Create your views here.
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer

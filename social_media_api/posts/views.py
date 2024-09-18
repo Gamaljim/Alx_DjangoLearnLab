e063443377a7a3b220eae6cb67cd350da6d52f03
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-18 21:35:09 +0400

    task 1 first commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,15 +1,36 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import PostSerializer, CommentSerializer
 from .models import Post, Comment
-# Create your views here.
 
 
-class PostListView(generics.ListAPIView):
+# Create your  here.
+
+
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-class PostDetailView(generics.RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get_object(self):
+        post = super().get_object()
+
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            if post.author != self.request.user:
+                raise PermissionDenied("You Do not have permission to modify this post")
+        return post
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_object(self):
+        comment = super().get_object()
+
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            if comment.author != self.request.user:
+                raise PermissionDenied('You do not have permission to modify this comment')
+        return comment

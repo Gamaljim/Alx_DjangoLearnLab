f34c7cc94802b6bfb16d84028903b2d4b9573f72
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-09-18 06:51:10 +0400

    trying first commit

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('email is required')
+        user = self.model(email=self.normmalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Must be staff')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Must be superuser')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    bio = models.CharField(max_length=250)
+    profile_picture = models.ImageField(blank=True, null=True)
+    followers = models.ManyToManyField('followers')
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
